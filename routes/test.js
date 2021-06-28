eb506ea0c100d99c2d9502bed7f1fd990fc659a6
Junsuk Oh <a811219@gmail.com>
2021-06-28 14:04:43 +0900

    put 전달코드

--- routes/test.js
+++ routes/test.js
@@ -3,9 +3,20 @@ var router = express.Router();
 var axios = require('axios');
 
 router.get('/', async function (req, res) {
-  let url = req.param('url');
+  let url = req.query.url;
 
   let response = await axios.get(`https://${url}`);
+  res.json(response.data);
+});
+
+router.put('/', async function (req, res) {
+  let url = req.query.url;
+
+  let response = await axios.put(`https://${url}`, req.body, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
   res.json(response.data);
 });
 
